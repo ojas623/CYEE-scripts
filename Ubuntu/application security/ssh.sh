f33719c19c82d560b089a68eddfcdf3367c42b9c
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-08 09:28:52 -0800

    added ssh

--- Ubuntu/application security/ssh.sh
+++ Ubuntu/application security/ssh.sh
@@ -1,15 +1,22 @@
-read -p "Do you want SSH installed? y/n: "
-if [[ $REPLY =~ ^[Nn]$ ]]
+echo "do you want SSH to be enabled?"
+read answer
+if [[answer -e "y"]]
 then
-  apt-fast autoremove -y --purge openssh-server ssh
+echo "enabling SSH..."
+sudo  apt-fast install openssh-client -y
+find /etc/ssh -xdev -type f -name 'ssh_host_*_key' -exec chown root:root {} \;
+find /etc/ssh -xdev -type f -name 'ssh_host_*_key' -exec chmod u-x,go-rwx {} \;
+find /etc/ssh -xdev -type f -name 'ssh_host_*_key.pub' -exec chmod u-x,go- wx {} \;
+find /etc/ssh -xdev -type f -name 'ssh_host_*_key.pub' -exec chown root:root {} \;
+cp ~/Linux/ssh/sshd_config /etc/ssh/sshd_config
+/usr/sbin/sshd -t
+systemctl restart sshd.service
+systemctl reload sshd
+fi
+echo "SSH enabled."
 else
-  sudo  apt-fast install openssh-client -y
-  find /etc/ssh -xdev -type f -name 'ssh_host_*_key' -exec chown root:root {} \;
-  find /etc/ssh -xdev -type f -name 'ssh_host_*_key' -exec chmod u-x,go-rwx {} \;
-  find /etc/ssh -xdev -type f -name 'ssh_host_*_key.pub' -exec chmod u-x,go- wx {} \;
-  find /etc/ssh -xdev -type f -name 'ssh_host_*_key.pub' -exec chown root:root {} \;
-  cp ~/Linux/ssh/sshd_config /etc/ssh/sshd_config
-  /usr/sbin/sshd -t
-  systemctl restart sshd.service
-  systemctl reload sshd
+
+echo "disabling SSH packages..."
+apt-fast autoremove -y --purge openssh-server ssh
+echo "done."
 fi
