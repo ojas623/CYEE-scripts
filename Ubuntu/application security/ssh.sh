4085eaf93ea742825d9c9345825a570ce5ab93f2
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-08 23:32:17 -0800

    fixed ssh

--- Ubuntu/application security/ssh.sh
+++ Ubuntu/application security/ssh.sh
@@ -2,17 +2,4 @@ echo "enabling SSH..."
 sudo systemctl enable ssh
 sudo systemctl start ssh
 echo "SSH enabled."
-fi
 
-echo "securing SSH..."
-sudo sed -i 's/^#Protocol 2/Protocol 2/' /etc/ssh/sshd_config
-sudo sed -i 's/^#LogLevel INFO/LogLevel VERBOSE/' /etc/ssh/sshd_config
-sudo sed -i 's/^#X11Forwarding yes/X11Forwarding no/' /etc/ssh/sshd_config
-sudo sed -i 's/^#MaxAuthTries 6/MaxAuthTries 4/' /etc/ssh/sshd_config
-sudo sed -i 's/^#IgnoreRhosts yes/IgnoreRhosts yes/' /etc/ssh/sshd_config
-sudo sed -i 's/^#HostbasedAuthentication no/HostbasedAuthentication no/' /etc/ssh/sshd_config
-sudo sed -i 's/^#PermitRootLogin yes/PermitRootLogin no/' /etc/ssh/sshd_config
-sudo sed -i 's/^#PermitEmptyPasswords no/PermitEmptyPasswords no/' /etc/ssh/sshd_config
-sudo sed -i 's/^#PermitUserEnvironment no/PermitUserEnvironment no/' /etc/ssh/sshd_config
-sudo sed -i 's/^#Ciphers aes256-ctr,aes192-ctr,aes128-ctr/Ciphers aes256-ctr,aes192-ctr,aes128-ctr/' /etc/ssh/sshd_config
-sudo sed -i 's/^#MACs
