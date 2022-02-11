91bccaf58736aa9df7bde63b3a10ed27ca07b4dc
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-11 09:14:54 -0800

    prohibitted files

---
+++ Windows 10/defensive countermeasures/prohibitFiles.bat
@@ -0,0 +1,94 @@
+@echo off
+setlocal enabledelayedexpansion
+net session
+
+echo "deleting prohibited files..."
+cd \
+
+del /s /q /f *.mp3
+del /s /q /f *.mp4
+del /s /q /f *.mov
+del /s /q /f *.gif
+del /s /q /f *.jpg
+del /s /q /f *.jpeg
+del /s /q /f *.png
+del /s /q /f *.wav
+del /s /q /f *.m4a
+del /s /q /f *.ogg
+del /s /q /f *.avi
+del /s /q /f *.flv
+del /s /q /f *.wmv
+del /s /q /f *.mkv
+del /s /q /f *.mpg
+del /s /q /f *.mpeg
+del /s /q /f *.m4v
+del /s /q /f *.3gp
+del /s /q /f *.3g2
+del /s /q /f *.webm
+del /s /q /f *.mpg
+del /s /q /f *.tif
+del /s /q /f *.gsm
+del /s /q /f *.flac
+del /s /q /f *.au
+del /s /q /f *.aiff
+del /s /q /f *.iff
+del /s /q /f *.aif
+del /s /q /f *.vox 
+del /s /q /f *.vob
+del /s /q /f *.swf
+del /s /q /f *.srt
+del /s /q /f *.rm
+del /s /q /f *.raw
+del /s /q /f *.wma
+del /s /q /f *.atrac
+del /s /q /f *.ra
+del /s /q /f *.ram
+del /s /q /f *.dss
+del /s /q /f *.dvf
+del /s /q /f *.oma
+del /s /q /f *.atp
+del /s /q /f *.mid
+del /s /q /f *.ape
+del /s /q /f *.ac3
+del /s /q /f *.m4p
+del /s /q /f *.midi
+del /s /q /f *.mp2
+del /s /q /f *.m3u
+del /s /q /f *.vqf
+del /s /q /f *.bmp
+del /s /q /f *.mpeg3
+del /s /q /f *.dma
+del /s /q /f *.aac 
+del /s /q /f *.aifc
+del /s /q /f *.wm
+del /s /q /f *.asx
+del /s /q /f *.wax
+del /s /q /f *.wvx
+del /s /q /f *.wmx
+del /s /q /f *.dvr-ms
+del /s /q /f *.wmd
+del /s /q /f *.mpeg
+del /s /q /f *.m1v
+del /s /q /f *.mpa
+del /s /q /f *.mpe
+del /s /q /f *.rmi
+del /s /q /f *.aif
+del /s /q /f *.aifc
+del /s /q /f *.snd
+del /s /q /f *.cda
+del /s /q /f *.ivf
+del /s /q /f *.m4v
+del /s /q /f *.m4pv
+del /s /q /f *.3g2
+del /s /q /f *.3gp2
+del /s /q /f *.3gp
+del /s /q /f *.adts
+del /s /q /f *.m2ts
+del /s /q /f *.hdp
+del /s /q /f *.wdp
+del /s /q /f *.aa
+del /s /q /f *.aax
+del /s /q /f *.aac
+del /s /q /f *.alac
+
+echo "prohibited files deleted."
