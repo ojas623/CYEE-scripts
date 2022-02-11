08971d8e522e24d404a767be0bce1186bea20edd
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-11 10:53:26 -0800

    update mis

--- Windows 10/misc.bat
+++ Windows 10/misc.bat
@@ -2,11 +2,23 @@
 setlocal enabledelayedexpansion
 net session
 
-echo Clearing DNS cache...
+echo "Clearing DNS cache..."
 ipconfig /flushdns
 echo DNS cache cleared
 
 sc config Server start=auto
 net start Server
 echo "Server service started"
+
+echo "enabling data execution prevention..."
+
+BCDEDIT /SET {CURRENT} NX ALWAYSON
+
+echo "data execution prevention enabled"
+
+echo "changing file system to NFTS..."
+
+convert C: /fs:ntfs  
+
+echo "file system changed"
 
