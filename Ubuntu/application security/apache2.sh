fc6adaf06d04ae00b626afd934ae1eb54a8c338a
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-16 10:38:13 -0800

    apache2 configuration

--- Ubuntu/application security/apache2.sh
+++ Ubuntu/application security/apache2.sh
@@ -1,4 +1,17 @@
 sudo apt update
-sudo apt install apache2
+apt-fast install apache2
 echo "Apache2 installed."
+
+apache2 -version
+echo "Apache is confirmed to be version 2"
+
+echo "allowing apache through firewall"
+sudo ufw allow "Apache"
+echo "Apache allowed through firewall"
+
+echo "starting apache"
+sudo systemctl start apache2
+echo "apache started"
+
+
 
