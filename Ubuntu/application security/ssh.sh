b7c7bbefd2e86ca10b20217d567110aa20f3a29b
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-16 10:21:35 -0800

    add more config options

--- Ubuntu/application security/ssh.sh
+++ Ubuntu/application security/ssh.sh
@@ -1,5 +1,10 @@
 echo "enabling SSH..."
 apt-fast install openssh-server
 apt-fast install openssh-client
+sudo service ssh restart
 systemctl reload sshd
 echo "SSH enabled."
+
+echo "disabling SSH connections from all other hosts"
+sudo iptables -A INPUT -p tcp --dport 8908 -j DROP
+echo "SSH connections from all other hosts blocked."
