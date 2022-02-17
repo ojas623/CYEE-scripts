8f063cae92695cda467f5918a12ac0b14de326df
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-16 22:45:47 -0800

    fixed firewall

--- Ubuntu/defensive countermeasures/firewall.sh
+++ Ubuntu/defensive countermeasures/firewall.sh
@@ -15,8 +15,19 @@ ufw deny in from ::1 #denies all IPv6 loopback traffic
 echo "configuring outbound rules..."
 ufw allow out on all
 ufw status numbered
-ufw default deny outgoing
+ufw default deny incoming
+ufw default allow outgoing
 ufw default deny routed
+
+echo "configuring other firewall rules..."
+ufw logging on
+ufw logging high
+
+echo "allowing internet access..."
+sudo ufw allow out 53  
+sudo ufw allow out http  
+sudo ufw allow out https  
+sudo ufw allow out 8080 
 
 #CIS 3.5.2
 
