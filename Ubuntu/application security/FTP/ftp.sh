856cd92d4841e8abd75d6ca24962d24989f5aff7
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-22 23:12:40 -0800

    organize, add ftp

---
+++ Ubuntu/application security/FTP/ftp.sh
@@ -0,0 +1,8 @@
+echo "enabling FTP..."
+sudo apt-get install proftpd
+sudo service proftpd restart
+sudo systemctl reload proftpd
+cp ~/CYEE-Scripts/Ubuntu/application security/FTP/proftpd.conf /etc/proftpd/proftpd.conf
+
+
+echo "FTP enabled."
