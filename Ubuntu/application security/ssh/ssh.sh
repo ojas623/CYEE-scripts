694af9f0d07aa42d54a2748b06d80063da61ebb5
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-22 23:15:40 -0800

    fixed error

--- Ubuntu/application security/ssh/ssh.sh
+++ Ubuntu/application security/ssh/ssh.sh
@@ -1,8 +1,13 @@
 echo "enabling SSH..."
 apt-fast install openssh-server
 apt-fast install openssh-client
+
+cp ~/CYEE-Scripts/Ubuntu/application security/ssh/sshd_config /etc/ssh/sshd_config
+/usr/sbin/sshd -t
+
 sudo service ssh restart
 systemctl reload sshd
+
 echo "SSH enabled."
 
 echo "disabling SSH connections from all other hosts"
