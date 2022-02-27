f3cf5b624f7f576fc7e1d8f226cd00d078ac3e71
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-26 17:15:43 -0800

    more misc option

--- Ubuntu/misc.sh
+++ Ubuntu/misc.sh
@@ -4,3 +4,13 @@ systemctl daemon-reload
 rm /etc/systemd/system/ctrl-alt-del.target
 echo "disabled control-alt-delete"
 
+echo "updating grub"
+update-grub
+echo "updated grub"
+
+echo "installing silversearcher-ag"
+apt install silversearcher-ag
+echo "installed silversearcher-ag"
+
+echo "installig curl"
+apt-get install curl -y
