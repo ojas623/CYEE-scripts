58535e2fbc6d9b3ae8f8d80a017f1dc67a2c7f5a
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-02-26 23:27:31 -0800

    edit

--- Ubuntu/user accounts/acctType.sh
+++ Ubuntu/user accounts/acctType.sh
@@ -1,5 +1,16 @@
-echo "enter the name of the user you no longer want to be an administrator:"
+echo "enter the name of the user whose account type you want to change"
 read user
-echo "this user will now be a standard user"
-sudo deluser $user sudo
-echo "removed $user from sudo group"
+
+echo "enter the new account type for $user (standard or admin)"
+read acctType
+if [ $acctType = "standard" ]
+then
+    usermod -L $user
+
+elif [ $acctType = "admin" ]
+then
+    usermod -G sudo $user
+    
+else
+    echo "invalid account type"
+fi
