479b3fccb66214d0b8e7caea8b37c36f9aea2e5a
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-03-14 18:28:55 -0700

    update

--- Ubuntu/defensive countermeasures/services.sh
+++ Ubuntu/defensive countermeasures/services.sh
@@ -1,58 +1,29 @@
 #! /bin/bash
 
-apt install systemctl 
-echo "systemctl installed."
-
-apt install chrony
-echo "chrony installed."
-
-apt purge ntp
-echo "ntp removed."
-
-apt purge xserver-xorg*
-echo "X windows system packages removed."
-
-systemctl stop avahi-daemon.service
-systemctl stop avahi-daemon.socket
-apt purge avahi-daemon
-echo "avahi-daemon removed."
-
-apt purge cups
-echo "cups removed."
-
-apt purge isc-dhcp-server
-echo "DHCP server removed."
-#DHCP is dynamic host config protocol, allows machines to be dynamically assigned an IP
-
-apt purge sladp
-echo "sladp removed."
-#LDAP is light-weight directory access protocol, used to access a directory
-
-apt purge nfs-kernel-server
-echo "NFS server removed."
-#NFS is network file system, used to share files between computers
-
-apt purge bind9
-echo "DNS server removed."
-
-apt purge vsftpd
-echo "FTP server removed."
-
-apt purge apache2
-echo "Apache2 removed."
-
-apt purge dovecot-imapd dovecot-pop3d dovecot-sieve dovecot-lmtpd
-echo "IMAP, POP3, SIEVE, LMTP removed."
-
-apt purge samba
-echo "Samba removed."
-
-apt purge squid
-echo "Squid removed."
-#squid is a proxy server
-
-apt purge snmpd
-echo "SNMP server removed."
-#SNMP is simple netowrk management protocol, used to monitor and control network devices
-
-#CIS section 2.1
+sudo apt install systemctl 
+sudo apt install chrony
+sudo apt install crond
+sudo apt install auditd
+dpkg -s chrony
+sudo apt purge ntp
+sudo apt purge xserver-xorg*
+sudo apt purge cups
+sudo apt purge isc-dhcp-server
+sudo apt purge slapd
+sudo apt purge nfs-kernel-server
+sudo apt purge bind9
+sudo apt purge vsftpd
+sudo apt purge apache2
+sudo apt purge dovecot-imapd dovecot-pop3d
+sudo apt purge samba 
+sudo apt purge squid 
+sudo apt purge snmpd
+sudo apt purge rsync
+sudo apt purge nis
+sudo apt purge rsh-client
+sudo apt purge talk
+sudo apt purge telnet
+sudo apt purge ldap-utils
+sudo apt purge rpcbind
+sudo apt remove telnet-server
+sudo apt remove telnet-client
