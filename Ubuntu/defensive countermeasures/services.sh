c3136492ab589a4d3961f2c1b7326035c4c27e84
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-04-11 09:22:56 -0700

    refactor & stuff from CIS

--- Ubuntu/defensive countermeasures/services.sh
+++ Ubuntu/defensive countermeasures/services.sh
@@ -4,29 +4,128 @@ sudo apt install chrony
 sudo apt install crond
 sudo apt install auditd
 sudo apt install tcpd 
-dpkg -s chrony
-sudo apt purge ntp
-sudo apt purge xserver-xorg*
-sudo apt purge cups
-sudo apt purge isc-dhcp-server
-sudo apt purge slapd
-sudo apt purge nfs-kernel-server
-sudo apt purge bind9
-sudo apt purge vsftpd
-sudo apt purge apache2
-sudo apt purge dovecot-imapd dovecot-pop3d
-sudo apt purge samba 
-sudo apt purge squid 
-sudo apt purge snmpd
-sudo apt purge rsync
-sudo apt purge nis
-sudo apt purge rsh-client
-sudo apt purge talk
-sudo apt purge telnet
-sudo apt purge ldap-utils
-sudo apt purge rpcbind
-sudo apt purge telnet-server
-sudo apt purge telnet-client
-systemctl stop avahi-daaemon.service
-systemctl stop avahi-daemon.socket
-sudo apt purge avahi-daemon
+systemctl enable cron
+systemctl --now disable avahi-daemon
+systemctl --now disable cups
+systemctl --now disable isc-dhcp-server
+systemctl --now disable isc-dhcp-server6
+systemctl --now disable slapd
+systemctl --now disable nfs-server
+systemctl --now disable rpcbind
+systemctl --now disable bind9
+systemctl --now disable vsftpd
+systemctl --now disable apache2
+systemctl --now disable dovecot
+systemctl --now disable squid
+systemctl --now disable snmpd
+apt install systemctl -y
+systemctl disable xinetd
+apt purge xinetd
+systemctl stop xinetd
+apt-fast remove openbsd-inetd
+systemctl disable cups
+systemctl stop cups
+systemctl disable isc-dhcp-server
+systemctl stop isc-dhcp-server
+systemctl disable isc-dhcp-server6
+systemctl stop isc-dhcp-server6
+systemctl disable dhcpd
+systemctl stop dhcpd
+systemctl disable discard-dgram
+systemctl stop discard-dgram
+systemctl disable slapd
+systemctl stop slapd
+systemctl disable nfs-server
+systemctl stop nfs-server
+systemctl disable rpcbind
+systemctl stop rpcbind
+systemctl disable vsftpd
+systemctl stop vsftpd
+systemctl disable dovecot
+systemctl stop dovecot
+systemctl disable squid
+systemctl stop squid
+systemctl disable snmpd
+systemctl stop snmpd
+systemctl disable rsync
+systemctl stop rsync
+systemctl disable nis
+systemctl stop nic
+apt-fast remove nis -y
+systemctl stop nis
+apt-fast remove rsh-client rsh-redone-client
+apt-fast remove talk
+apt-fast remove telnet
+apt-fast remove ldap-utils
+systemctl enable auditd
+systemctl disable avahi-daemon
+systemctl stop avahi-daemon
+apt-fast remove rsh-server
+systemctl disable chargen-dgram
+systemctl stop chargen-dgram
+systemctl disable chargen-stream
+systemctl stop chargen-stream
+systemctl enable crond
+systemctl disable daytime-dgram
+systemctl stop daytime-dgram
+systemctl disable daytime-stream
+systemctl stop daytime-stream
+systemctl disable discard-dgram
+systemctl stop discard-dgram
+systemctl disable discard-stream
+systemctl stop discard-stream
+systemctl disable named
+systemctl stop named
+systemctl disable echo-stream
+systemctl stop echo-stream
+systemctl disable echo-dgram
+systemctl stop echo-dgram
+systemctl disable vsftpd
+systemctl stop vsftpd
+systemctl disable httpd
+systemctl stop httpd
+apt-fast remove openldap-clients -y
+systemctl disable nfs
+systemctl stop nfs
+apt-fast remove mctrans -y
+apt-fast remove ypbind -y
+systemctl disable ypserv
+systemctl stop ypserv
+apt-fast remove ypserv -y
+systemctl disable rpcbind
+systemctl stop rpcbind
+apt-fast remove rsh -y
+apt-fast remove rsh-client -y
+systemctl disable rexec.socket
+systemctl stop rexec.socket
+systemctl disable rlogin.socket
+systemctl stop rlogin.socket
+systemctl disable rsh.socket
+systemctl stop rsh.socket
+systemctl disable rsyncd
+systemctl stop rsyncd
+systemctl disable smb
+systemctl stop smb
+systemctl --now disable smbd
+apt-fast remove setroubleshoot -y
+systemctl disable snmpd
+systemctl stop snmpd
+systemctl disable minetest
+systemctl stop minetest
+systemctl disable ntalk
+systemctl stop ntalk
+apt-fast remove talk -y
+systemctl disable snmpd
+systemctl stop snmpd
+apt-fast remove telnet
+systemctl disable telnet.socket
+systemctl stop telnet.socket
+apt-fast remove telnet-server
+apt-fast remove tftp-server
+apt-fast remove tftp
+systemctl disable tftp
+systemctl stop tftp
+systemctl disable tftp.socket
+systemctl stop tftp.socket
+systemctl stop postgresql
+systemctl disable postgresql
