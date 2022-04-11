3452f533a5f9ca73c6f84d348a9eec989684f968
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-04-10 22:13:45 -0700

    network sec

---
+++ Ubuntu/networkSec.sh
@@ -0,0 +1,11 @@
+#use Vim and add this to the file below: install dccp /bin/true 
+# /etc/modprobe.d/dccp.conf
+
+#use Vim and add this to the file below: install sctp /bin/true 
+# /etc/modprobe.d/sctp.conf
+
+#use Vim and add this to the file below: install rds /bin/true 
+# /etc/modprobe.d/rds.conf
+
+#use Vim and add this to the file below: install tipc /bin/true 
+# /etc/modprobe.d/tipc.conf
