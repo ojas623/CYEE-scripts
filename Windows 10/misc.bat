6f9598926adc4a2e75bba8e275fb1ee77da674da
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-04-19 12:52:25 -0700

    misc security options added

--- Windows 10/misc.bat
+++ Windows 10/misc.bat
@@ -22,3 +22,18 @@ convert C: /fs:ntfs
 
 echo "file system changed"
 
+echo "enabling smart screen"
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v EnableWindowsSmartScreen /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v WindowsSmartScreenTimeout /t REG_DWORD /d 0 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v WindowsSmartScreenEnableHistory /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v WindowsSmartScreenEnableHistoryDetails /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v WindowsSmartScreenEnableInPrivateBrowsing /t REG_DWORD /d 1 /f
+reg add "HKEY_LOCAL_MACHINE\SOFTWARE\Policies\Microsoft\Windows\System" /v WindowsSmartScreenEnableInPrivateBrowsingDetails /t REG_DWORD /d 1 /f
+echo "smart screen enabled"
+
+echo "configuring screen timeout limit..."
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\0" /v "ACSettingIndex" /t REG_DWORD /d 15 /f
+reg add "HKEY_LOCAL_MACHINE\SYSTEM\CurrentControlSet\Control\Power\PowerSettings\1" /v "DCSettingIndex" /t REG_DWORD /d 15 /f
+echo "done"
+
+
