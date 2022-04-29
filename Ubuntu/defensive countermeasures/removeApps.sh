3d128db3120206e2bd6069998f45a02a80be10ec
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-04-28 23:12:20 -0700

    more apps

--- Ubuntu/defensive countermeasures/removeApps.sh
+++ Ubuntu/defensive countermeasures/removeApps.sh
@@ -2,3 +2,7 @@ apt-get purge wireshark -y
 apt-get purge tor -y
 apt-get purge thunderbird* -y
 apt-get purge nmap -y
+apt-get purge endless-sky -y
+apt-get purge gimp -y
+apt-get purge hexchat -y
+apt-get purge minetest -y
