8a4d5b2282962f07751163864172881bd0010734
Ojas Sathe <83601895+ojas623@users.noreply.github.com>
2022-05-01 00:57:32 -0700

    fix

--- Windows 10/policies/auditpol.bat
+++ Windows 10/policies/auditpol.bat
@@ -2,15 +2,6 @@
 setlocal enabledelayedexpansion
 net session
 
-if %errorlevel%==0 (
-	echo Admin rights granted!
-   )
-   else (
-    echo Failure, no rights
-	pause
-    exit
-)
-
 
 echo "setting audit policy..."
 auditpol /set /category:* /success:enable
